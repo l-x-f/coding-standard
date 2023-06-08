1ca97b039a8c553acbf9244c97ee165cbbbf2184
xiaofei <lixiaofei0627@gmail.com>
2023-06-08 11:03:30 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,14 @@ npm run build
 
 cd .vitepress/dist
 
-# local
-echo "local deploy..."
-cp -r ./ C:/nginx/html/standard/coding-standard
-
 git init
 git add -A
 git commit -m 'deploy'
 
 git push -f https://github.com/l-x-f/coding-standard.git/ master:gh-pages
+
+# local
+echo "local deploy..."
+cp -rf ./ C:/nginx/html/standard/coding-standard
 
 cd -
