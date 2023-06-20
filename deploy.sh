20a2299a6c05659c9a3db0748432ed99e5d87bf6
xiaofei <lixiaofei0627@gmail.com>
2023-06-20 09:17:39 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,12 @@ git add -A
 git commit -m 'deploy'
 
 git push -f https://github.com/l-x-f/coding-standard.git/ master:gh-pages
+echo -e "\n\033[32mSuccessfully deploy gh-pages\033[0m"
 
 # local
-echo "local deploy..."
+echo -e "\nlocal deploy..."
 cp -rf ./ C:/nginx/html/standard/coding-standard
+echo -e "\n\033[32mSuccessfully deploy local\033[0m"
 
 cd -
+echo "✅ Deploy completed"
